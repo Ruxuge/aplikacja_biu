1861a6ccdb71a4744dac3e14e3ecb09d212824d1
Filip Werra <filipwerra.12@gmail.com>
2021-02-07 00:29:01 +0100

    add possible request

---
+++ src/Request/SendPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SendPost(yourUrl, body) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", {yourUrl}, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        value1: value,
+        value2: value
+    }));
+}
---
+++ src/components/Admin/Views/ViewJob.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function View({ idNumber, visibleItem, by, text, title }) {
+    return (
+        <div>
+            {visibleItem &&
+            <article>
+                <p>Id number of edited job: {idNumber}</p>
+                <p>Title: {title}</p>
+                <p>Text: {text}</p>
+                <p>By: {by}</p>
+            </article>}
+        </div>
+    )
+}
