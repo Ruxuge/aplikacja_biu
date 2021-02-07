211497d9ff36ea1f850188bb1588675effa742f7
Filip Werra <filipwerra.12@gmail.com>
2021-02-07 02:29:47 +0100

    create pointing

--- src/components/New/Topicality.js
+++ src/components/New/Topicality.js
@@ -2,41 +2,75 @@ import React from "react";
 import "./Topicality.scss";
 import useGet from "../../hooks/useGet";
 import useAddedAgo from "../../hooks/useAddedAgo";
-import Discuss from "../Comments/Discuss";
 import {Link} from "react-router-dom";
 
-export default function Topicality({ topicalityId }) {
+export default function Topicality({topicalityId}) {
     const URI = `https://hacker-news.firebaseio.com/v0/item/${topicalityId}.json?print=pretty`;
     const topicality = useGet(URI, {});
 
-    const timeNotNull = () => topicality !== null ? topicality.time : 0;
-    const time = useAddedAgo(timeNotNull())
+    const timeNotNull = () => topicality !== undefined ? topicality.time : 0;
+    const scoreNotNull = () => topicality !== null ? topicality.score : 0;
 
+    const time = useAddedAgo(timeNotNull());
 
-    const [hide, setHide] = React.useState(false);
-    const [vote, setVote] = React.useState(false);
+    const scoreRef = React.useRef(null);
 
+    function reducer(state, action) {
+        switch (action.type) {
+            case 'VOTE':
+                return {
+                    ...state,
+                    vote: true,
+                    score: scoreRef.current + 1
+                }
+            case 'UNVOTE':
+                return {
+                    ...state,
+                    vote: false,
+                    score: state.score - 1
+                }
+            case 'HIDE':
+                return {
+                    ...state,
+                    hide: true
+                }
+            default:
+                throw new Error();
+        }
+    }
 
-    return <>
-        {!hide && <li className="list">
-            {topicality !== null &&
-            <main className='post'>
-                <div className='arrow'
-                     onClick={() => setVote(true)}
-                     style={{opacity: vote ? 0 : 1}}
-                />
-                <div className="content">
-                    <a href={topicality.url}>{topicality.title}</a>
-                    <p>{topicality.score} {topicality.score === 1 ? 'point' : 'points'} by {topicality.by} {time} |&nbsp;
-                        <span
-                            onClick={() => setVote(false)}
-                            style={{display: vote ? 'inline-block' : 'none'}} > unvote | </span>
-                        <span onClick={() => setHide(true)}>hide | </span>
-                        <a href="">past | </a>
-                        <Link to={`/item?id=${topicality.id}`}>discuss</Link>
-                    </p>
-                </div>
-            </main>}
-        </li>}
-    </>;
+    React.useEffect(() => {
+        scoreRef.current = scoreNotNull()
+    }, [scoreRef.current, scoreNotNull])
+
+    const [state, dispatch] = React.useReducer(reducer, {
+        voted: false,
+        score: scoreRef.current,
+        hide: false
+    });
+
+    return (
+        <>
+            {topicality !== null && <>
+                {!state.hide && <li className="list">
+                    <main className='post'>
+                        <div className='arrow'
+                             onClick={() => dispatch({type: 'VOTE'})}
+                             style={{opacity: state.vote ? 0 : 1}}/>
+                        <div className="content">
+                            <a href={topicality.url}>{topicality.title}</a>
+                            <p ref={scoreRef}>{state.score === null ? topicality.score : state.score} {(topicality.score === 1) ? 'point' : 'points'} by {topicality.by} {time} |&nbsp;
+                                <span
+                                    onClick={(() => dispatch({type: 'UNVOTE'}))}
+                                    style={{display: state.vote ? 'inline-block' : 'none'}}>unvote | </span>
+                                <span onClick={() => dispatch({type: 'HIDE'})}>hide | </span>
+                                <a href="">past | </a>
+                                <Link to={`/item?id=${topicality.id}`}>discuss</Link>
+                            </p>
+                        </div>
+                    </main>
+                </li>}
+            </>}
+        </>
+    );
 }
